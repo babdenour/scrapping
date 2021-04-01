8c7512cd774c7b48b67d545df53df795b07a958c
bamba-salim <01salim.bamba@gmail.com>
2021-04-02 01:36:28 +0200

    - scrapping fonctionelle -> ok avec le site "NDRT" - readme -> à compléter - boucle pour récuper les autres pasges -> en cours

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 console.log("start");
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
+const month = '09';
+const year = '2020';
 
+
+const category = [
+  {
+    name: 'wallet'
+  },
+  {
+    name: 'hat'
+  },
+  {
+    name: 'bag'
+  },
+  {
+    name: 'socks'
+  }
+]
+
+const ndrt = 'http://ndrt.alkebulabz.com/category?cat=hat';
 (async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto(
-		"https://www.footlocker.fr/fr/category/all/jordan/jordan-1.html"
-	);
-	const sneakers = await page.evaluate(() => {
-		let snkrs = [];
-		let list = document.querySelector("div.ProductCard");
-		console.log(list);
-		//let els = document.querySelectorAll("div.fl-product-productlist--item");
-		//*[@id="main"]/div/div[2]/div/section/div/div[2]/ul[1]/li[6]/div[2]
-		// els.forEach((el) => {
-		// 	(list = el
-		// 		.querySelector("div.fl-product-tile--name")
-		// 		?.textContent.trim()),
-		// 		snkrs.push({
-		// 			title: el
-		// 				.querySelector("div.fl-product-tile--name")
-		// 				?.textContent.trim(),
-		// 			url: el.querySelector("div.fl-product-tile--basic a")?.textContent,
-		// 			img: el.querySelector("picture.fl-picture")?.src,
-		// 		});
-		// console.log(list);
-		//	});
-		return snkrs;
-	});
-	console.log("snkrs", sneakers);
-
-	await browser.close();
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto(ndrt);
+  const gears = await page.evaluate(() => {
+    let movies = [];
+    // let elements = document.querySelectorAll('div.list_item');
+    let hats = document.querySelectorAll('.ndrt-hover');
+    for (hat of hats) {
+      movies.push({
+        img: hat.querySelector('img')?.src,
+        name: hat.querySelector('p.h4')?.textContent.trim(),
+        price: parseFloat(hat.querySelector('p.text-gold')?.textContent.trim().slice(0,-2)),
+        collection: hat.querySelector('em')?.textContent.trim()
+      })
+    }
+    return movies;
+  });
+  
+  gears.push({test: 'test'})
+  console.log(gears);
+  await browser.close();
 })();
---
+++ readme.md
@@ -0,0 +1,30 @@
+<h1>SCRAPPING DU SHOP NDRT</h1>
+<em>Développé par Abdenour BENSOUNA et Salim Bamba</em>
+
+<hr/>
+
+<p>
+BLabla
+</p>
+
+<ul>
+    <li>Node Js (<a href="https://nodejs.org/fr/" title="Liens vers la doc" style='text-decoration: none'>👁</a>)</li>
+    <li>Puppeteer (<a href="https://pptr.dev/" title="Liens vers la doc" style='text-decoration: none'>👁</a>)</li>
+    <li>MongoDB (<a href="https://www.mongodb.com/" title="Liens vers la doc" style='text-decoration: none'>👁</a>)</li>
+</ul>
+
+<hr/>
+
+Pour bien faire fonctionner le projet c'est simple:
+<ol>
+    <li>npm i</li>
+    <li>npm start</li>
+</ol>
+<hr/>
+<p>
+Pour nous appuyer tout au long de ce projet nous avons suivis les tutos de :
+</p>
+<ul>
+    <li><a href="https://youtu.be/Y_NlDyUfVJ8" title="Liens vers le tuto" style='text-decoration: none; color:pink'>Romain Kania</a></li>
+    <li><a href="https://youtu.be/tLIqYdKhwSc" title="Liens vers le tuto" style='text-decoration: none; color:pink'>Captain Dev</a></li>
+</ul>
