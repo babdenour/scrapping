c51a91763519dc8f569ccb70decd48bceafeadfc
babdenour <abde1802@hotmail.fr>
2021-04-02 15:01:00 +0200

    db in tha place

---
+++ dataModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//Creat Shema and model
+const ItemSchema = new Schema({
+	img: String,
+	name: String,
+	price: Number,
+	collection_name: String,
+	category: String,
+});
+
+const Item = mongoose.model("item", ItemSchema);
+
+module.exports = Item;
--- index.js
+++ index.js
@@ -1,34 +1,85 @@
-console.log("start");
 const puppeteer = require("puppeteer");
+const mongoose = require("mongoose");
+const Item = require("./dataModel");
+
+//connection to db
+mongoose.connect("mongodb://localhost:27017/scrappingdb", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+mongoose.connection
+	.once("open", function () {
+		console.log("Conection has been made");
+	})
+	.on("error", function (error) {
+		console.log("Connection error", error);
+	});
+
+//debut du scrap
+console.log("Début du scrapping");
+
+let catalog = [];
 
 (async () => {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
-	await page.goto(
-		"https://www.footlocker.fr/fr/category/all/jordan/jordan-1.html"
-	);
-	const sneakers = await page.evaluate(() => {
-		let snkrs = [];
-		let list = document.querySelector("div.ProductCard");
-		console.log(list);
-		//let els = document.querySelectorAll("div.fl-product-productlist--item");
-		//*[@id="main"]/div/div[2]/div/section/div/div[2]/ul[1]/li[6]/div[2]
-		// els.forEach((el) => {
-		// 	(list = el
-		// 		.querySelector("div.fl-product-tile--name")
-		// 		?.textContent.trim()),
-		// 		snkrs.push({
-		// 			title: el
-		// 				.querySelector("div.fl-product-tile--name")
-		// 				?.textContent.trim(),
-		// 			url: el.querySelector("div.fl-product-tile--basic a")?.textContent,
-		// 			img: el.querySelector("picture.fl-picture")?.src,
-		// 		});
-		// console.log(list);
-		//	});
-		return snkrs;
-	});
-	console.log("snkrs", sneakers);
 
+	await page.goto(`http://ndrt.alkebulabz.com/`);
+	const categories = await page.evaluate(() => {
+		let categoryList = [];
+		// find categorie
+		let categories = document.querySelectorAll(".card");
+		// get categories name
+		for (category of categories) {
+			categoryList.push({
+				name: category.querySelector("p.h5")?.textContent.trim(),
+			});
+		}
+		return categoryList;
+	});
+
+	for (category of categories) {
+		await page.goto(`http://ndrt.alkebulabz.com/category?cat=${category.name}`);
+		const productList = await page.evaluate(() => {
+			let productList = [];
+
+			// get products from
+			let products = document.querySelectorAll(".mb-1");
+			for (product of products) {
+				// get product information to create object
+				productList.push({
+					img: product.querySelector("img")?.src,
+					name: product.querySelector("p.h4")?.textContent.trim(),
+					price: parseFloat(
+						product
+							.querySelector("p.text-gold")
+							?.textContent.trim()
+							.replaceAll("€", "")
+							.replaceAll(" ", "")
+					),
+					collection_name: product.querySelector("em")?.textContent.trim(),
+				});
+			}
+			return productList;
+		});
+
+		// add category name in product object
+		productList.forEach((el) => {
+			el.category = category.name;
+			catalog.push(el);
+		});
+	}
+
+	console.log(catalog);
+
+	//save in db
+	catalog.forEach((el) => {
+		let item = new Item(el);
+		item.save();
+	});
+
+	//close the browser
 	await browser.close();
+	console.log("Fin du scrapping");
 })();
